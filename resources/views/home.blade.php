7905f66c24b70f08a4c6b2005c48be4420c7609f
Nurvia Sulistry <nurviasulistry@gmail.com>
2025-11-17 16:27:24 +0700

    TentangRiauport

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -475,7 +475,34 @@
         <div class="w-3 h-3 bg-white border border-black rounded-full"></div>
     </div>
 
-    
+<section>
+    <div class="relative flex flex-col md:flex-row max-w-5xl mx-auto shadow-2xl rounded-lg my-12">
+
+      <div class="relative z-10 w-full md:w-2/5 bg-riau-blue text-white p-10 rounded-t-lg md:rounded-l-lg md:rounded-tr-none md:rounded-r-[3rem]">
+        
+        <h2 class="text-3xl font-bold mb-6" style="font-family: serif;">Tentang RiauPort</h2>
+        
+        <p class="text-sm font-bold mb-4">
+          Sistem Informasi Layanan Travel berbasis website berfungsi untuk mempermudah masyarakat dalam memperoleh informasi terkait perjalanan travel, khususnya untuk kebutuhan mudik.
+        </p>
+        
+        <p class="text-sm font-bold mb-4">
+          sistem berbasis website yang terpusat diperlukan untuk menyediakan layanan informasi travel. Karena website ini memang dirancang sedemikian rupa, pengguna dapat dengan mudah menemukan berbagai jasa travel sesuai daerah pilihan mereka. Nama travel, jadwal keberangkatan, destinasi, harga, jenis kendaraan, serta kontak yang dapat dihubungi termasuk ke dalam informasi yang disajikan.
+        </p>
+      </div>
+
+      <div class="w-full md:w-3/5 bg-white p-10 rounded-b-lg md:rounded-r-lg md:rounded-bl-none flex flex-col items-center justify-center">
+        
+        <img src="http://127.0.0.1:8000/riauport-logo.png" alt="riauport-logo.png" class="w-64 mb-4">
+        
+        <p class="text-md text-riau-teal text-center font-im-fell">
+          Temukan berbagai Travel dalam satu platform RiauPort
+        </p>
+      </div>
+
+    </div>
+  </section>
+
     {{-- ================= SLIDESHOW SCRIPT ================= --}}
     <script>
         (function() {
