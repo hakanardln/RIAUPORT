ccb49917ebd8b9e8c880f5b347feeb9ab50f8fc4
hakanardln <handalkaris.arbi@yahoo.com>
2025-11-18 15:18:00 +0700

    New

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="id">
 
-<>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>RiauPort – Data Pelanggan</title>
